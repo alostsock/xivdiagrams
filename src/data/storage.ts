ddd75eba1c57f4323c0e4f7cf29e10813960dada
alostsock <49344439+alostsock@users.noreply.github.com>
2021-11-24 01:04:37 -0500

    fix key cache to be LRU

--- src/data/storage.ts
+++ src/data/storage.ts
@@ -10,27 +10,38 @@ export function getAllKeys(): KeyCache | null {
 
 export function getKey(planId: string): string | null {
 	const keycache = getAllKeys();
-	const cached = keycache?.find(([id]) => id === planId);
-	return cached?.[1] ?? null;
+	if (!keycache) return null;
+
+	// move recently used keys to the front
+	const cached = removeKey(planId);
+	if (!cached) return null;
+	storeKey(...cached);
+	return cached[1];
 }
 
-export function storeKey(planId: string, editKey: string) {
+export function storeKey(planId: string, editKey: string): void {
 	const keycache = getAllKeys() || [];
-	const cached = keycache?.find(
-		([id, key]) => id === planId && key === editKey
-	);
+	if (!keycache) return;
+
+	const cached = keycache.find(([id, key]) => id === planId && key === editKey);
 	if (!cached) {
 		keycache.unshift([planId, editKey]);
 		storage.setItem('editKeys', JSON.stringify(keycache.slice(0, keyLimit)));
 	}
 }
 
-export function removeKey(planId: string) {
+export function removeKey(
+	planId: string
+): [planId: string, editKey: string] | null {
 	const keycache = getAllKeys();
-	if (!keycache) return;
+	if (!keycache) return null;
+
 	const index = keycache.findIndex(([id]) => id === planId);
 	if (index !== -1) {
-		keycache.splice(index, 1);
+		const [removed] = keycache.splice(index, 1);
 		storage.setItem('editKeys', JSON.stringify(keycache));
+		return removed;
+	} else {
+		return null;
 	}
 }
